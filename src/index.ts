0ffbe41bacd9887add9688e1458d50bc6a1c61ee
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2025-06-03 13:51:37 +0330

    refactor: enhance main function to trim whitespace from API keys and improve error handling

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,30 @@ import { getDiff } from "./github.utils";
 import { getAIResult, getPrompt } from "./gemini.utils";
 
 async function main() {
-  const githubToken = core.getInput("GITHUB_TOKEN", { required: true });
-  const geminiApiKey = core.getInput("GEMINI_API_KEY", { required: true });
+  try {
+    const githubToken = core.getInput("GITHUB_TOKEN", {
+      required: true,
+      trimWhitespace: true,
+    });
+    const geminiApiKey = core.getInput("GEMINI_API_KEY", {
+      required: true,
+      trimWhitespace: true,
+    });
 
-  const gemini = new GoogleGenAI({
-    apiKey: geminiApiKey,
-  });
-  const octokit = github.getOctokit(githubToken);
+    const gemini = new GoogleGenAI({
+      apiKey: geminiApiKey,
+    });
+    const octokit = github.getOctokit(githubToken);
 
-  const diff = await getDiff(octokit, github.context);
+    const diff = await getDiff(octokit, github.context);
 
-  const prompt = await getPrompt(diff);
-  const result = await getAIResult(gemini, prompt);
+    const prompt = await getPrompt(diff);
+    const result = await getAIResult(gemini, prompt);
 
-  console.log(result);
+    result && core.info(result);
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error);
+  }
 }
 
 main();
